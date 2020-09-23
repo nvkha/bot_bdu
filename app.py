43d681b38732f6f69b08bf0c14fca27611666aff
nvkha <nvkha97@gmail.com>
2020-09-23 21:07:35 +0700

    change app.py

--- app.py
+++ app.py
@@ -41,9 +41,11 @@ def webhook_handle():
         "tuần này học gì", "tuần này học gì nhỉ", "tuần này học gì ku"]
     
     data = request.get_json()
-   
+    print(data)
+    '''
     message = data['message']
     sender_id = data['sender']['id']
+    
     # Check if student in database 
     student = db.execute('SELECT * FROM users WHERE id=?', sender_id)
     if not student:
@@ -127,7 +129,7 @@ def webhook_handle():
         send_mess(sender_id, "Bây giờ bạn có thể hỏi mình một số thông tin về lịch học rồi đó ^_^")
     else:
         send_mess(sender_id, "Xin lỗi mình không đủ thông minh để hiểu đước ý của bạn !")
-
+    '''
     return 'ok'
     
     
